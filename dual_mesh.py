bf9d5967f143b4db78c04d9272cf82116024835b
bartosz.bartmanski <bartosz.bartmanski@maths.ox.ac.uk>
2019-11-13 11:22:01 +0100

    Made the dual_mesh module more general - it calculates the dual mesh for non-triangular meshes.

--- dual_mesh.py
+++ dual_mesh.py
@@ -61,12 +61,11 @@ def get_dual_points(mesh, index):
 
     """
     assert isinstance(mesh, meshio._mesh.Mesh)
-    _cell_types = ("vertex", "line", "triangle")
     ## For each type of cell do the following
     # Find the cells where the given index appears
-    _idxs = [np.where(mesh.cells[x] == index)[0] for x in _cell_types]
+    _idxs = [np.where(mesh.cells[x] == index)[0] for x in mesh.cells]
     # Find the centers of all the cells
-    _vs = [mesh.points[mesh.cells[x][_idxs[i]]].mean(axis=1) for i,x in enumerate(_cell_types)]
+    _vs = [mesh.points[mesh.cells[x][_idxs[i]]].mean(axis=1) for i,x in enumerate(mesh.cells)]
     return np.concatenate(_vs, axis=0)
 
 
